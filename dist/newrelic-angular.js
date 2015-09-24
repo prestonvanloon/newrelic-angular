ac7b67c3a37a7977b38bc4c59ea0cc58585b0f4d
Antonio Essex-Lettieri <alettieri@gmail.com>
2015-09-23 22:03:55 -0700

    Update(Dist) Update newrelic-angular

--- dist/newrelic-angular.js
+++ dist/newrelic-angular.js
@@ -1,5 +1,8 @@
 angular.module('newrelic-angular', [
-  'newrelic-angular.decorator.log'
+  'angulartics',
+  'angulartics.newrelic.insights',
+  'newrelic-angular.decorator.log',
+  'newrelic-angular.interceptor.http'
 ]);
 
 /*
@@ -52,8 +55,53 @@ angular.module('newrelic-angular', [
 
 })();
 
+(function (angular) {
+  	'use strict';
+	
+	angular.module('newrelic-angular.interceptor.http', []).factory('httpInterceptor', HttpInterceptorFactory)
+    .config(HttpInterceptorConfig)
+  ;
+	
+  /**
+   * Http Interceptor
+   * Intercepts HTTP response and request errors and logs them to new relic
+   */
+  HttpInterceptorFactory.$inject = ['$q', '$log'];
+  function HttpInterceptorFactory($q, $log) {
+
+    // Intercept request and response errors
+    return {
+      requestError: logAndReturnError.bind(null, 'request'),
+      responseError: logAndReturnError.bind(null, 'response')
+    };
+
+    /**
+     * Log and return rejection promise
+     */
+    function logAndReturnError(httpType, rejection) {
+      var config = rejection.config || {};
+      var err = 'bad ' + config.method + ' ' + httpType + ' ' + rejection.config.url;
+      $log.error(new Error(err));
+      return $q.reject(rejection);
+    }
+
+  }
+	
+  /**
+   * Intercept any Http response or request errors
+   *   and log them to new relic
+   */
+  HttpInterceptorConfig.$inject = ['$httpProvider'];
+  function HttpInterceptorConfig($httpProvider) {
+    $httpProvider.interceptors.push('httpInterceptor');
+  }
+	
+	
+
+})(window.angular);
+
 /**
- * @license Angulartics v0.19.2
+ * @license Angulartics v0.17.2
  * (c) 2013 Luis Farzati http://luisfarzati.github.io/angulartics
  * License: MIT
  */
@@ -230,13 +278,7 @@ angular.module('angulartics', [])
         /* Add the full route to the base. */
         $analytics.settings.pageTracking.basePath = $window.location.pathname + "#";
       }
-      var noRoutesOrStates = true;
       if ($injector.has('$route')) {
-        var $route = $injector.get('$route');
-        for (var route in $route.routes) {
-          noRoutesOrStates = false;
-          break;
-        }
         $rootScope.$on('$routeChangeSuccess', function (event, current) {
           if (current && (current.$$route||current).redirectTo) return;
           var url = $analytics.settings.pageTracking.basePath + $location.url();
@@ -244,21 +286,9 @@ angular.module('angulartics', [])
         });
       }
       if ($injector.has('$state')) {
-        noRoutesOrStates = false;
         $rootScope.$on('$stateChangeSuccess', function (event, current) {
           var url = $analytics.settings.pageTracking.basePath + $location.url();
           $analytics.pageTrack(url, $location);
-        });
-      }
-      if (noRoutesOrStates) {
-        $rootScope.$on('$locationChangeSuccess', function (event, current) {
-          if (current && (current.$$route || current).redirectTo) return;
-          if ($analytics.settings.pageTracking.trackRelativePath) {
-            var url = $analytics.settings.pageTracking.basePath + $location.url();
-            $analytics.pageTrack(url, $location);
-          } else {
-            $analytics.pageTrack($location.absUrl(), $location);
-          }
         });
       }
     }]);
@@ -339,7 +369,7 @@ angular.module('angulartics', [])
 })(angular);
 
 /**
- * @license Angulartics v0.19.2
+ * @license Angulartics v0.17.2
  * (c) 2013 Luis Farzati http://luisfarzati.github.io/angulartics
  * Contributed by Jakub Hampl http://gampleman.eu
  * License: MIT
