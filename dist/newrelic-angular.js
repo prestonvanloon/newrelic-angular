9e984a5fc896016bb457d92f8e499a31f067deb1
Antonio Essex-Lettieri <alettieri@gmail.com>
2015-09-24 11:34:04 -0700

    Update dist

--- dist/newrelic-angular.js
+++ dist/newrelic-angular.js
@@ -1,104 +1,115 @@
-'use strict';
-
-angular.module('newrelic-angular', [
-  'angulartics',
-  'angulartics.newrelic.insights',
-  'newrelic-angular.decorator.log',
-  'newrelic-angular.interceptor.http'
-]);
-
-/*
- * Angular exceptionHandler delegates unhandled exceptions to $log.error
- * This module reports errors to New Relic Browser API
- */
-(function () {
   'use strict';
-  angular.module('newrelic-angular.decorator.log', [])
-    .config(logDecorator);
+  
+  angular.module('newrelic-angular', [
+    'angulartics',
+    'angulartics.newrelic.insights',
+    'newrelic-angular.decorator.log',
+    'newrelic-angular.interceptor.http'
+  ]);
 
-  logDecorator.$inject = ['$provide'];
-
-  function logDecorator($provide) {
-    $provide.decorator('$log', extendLog);
-  }
-
-  extendLog.$inject = ['$delegate', '$window'];
-
-  function extendLog($delegate, $window) {
-    $delegate.error = decorateErrorFunction($delegate.error);
-    $delegate.error.logs = []; // Restore logs
-    return $delegate;
-
-    function decorateErrorFunction(originalFn) {
-      return function () {
-
-        if (angular.isDefined($window.newrelic)) {
-          for (var arg in Array.prototype.slice.call(arguments)) {
-            if (arguments[arg] instanceof Error === false) {
-              arg = argToErrorObject(arg);
+  'use strict';
+  /*
+  * Angular exceptionHandler delegates unhandled exceptions to $log.error
+  * This module reports errors to New Relic Browser API
+  */
+  (function(angular) {
+    angular.module('newrelic-angular.decorator.log', [])
+      .config(logDecorator);
+  
+    logDecorator.$inject = ['$provide'];
+  
+    function logDecorator($provide) {
+      $provide.decorator('$log', extendLog);
+    }
+  
+    extendLog.$inject = ['$delegate', '$window'];
+  
+    function extendLog($delegate, $window) {
+      $delegate.error = decorateErrorFunction($delegate.error);
+      
+      return $delegate;
+  
+      function decorateErrorFunction(originalFn) {
+        
+        var fn = function() {
+  
+          if (angular.isDefined($window.newrelic)) {
+            for (var arg in Array.prototype.slice.call(arguments)) {
+              if (arguments[arg] instanceof Error === false) {
+                arg = argToErrorObject(arg);
+              }
+              $window.newrelic.noticeError(arg); // Report to NR
             }
-            $window.newrelic.noticeError(arg); // Report to NR
+          } else {
+            $delegate.warn('Could not report error to New Relic because the global New Relic module is missing!');
           }
-        } else {
-          $delegate.warn('Could not report error to New Relic because the global New Relic module is missing!');
-        }
-        originalFn.apply(null, arguments);
-      };
+          originalFn.apply(null, arguments);
+        };
+        
+        fn.logs = []; // Restore logs
+        
+        return fn;
+      }
     }
-  }
-
-  function argToErrorObject(arg) {
-    try {
-      throw new Error(arg);
-    } catch (e) {
-      return e;
-    }
-  }
-
-})();
-
-(function (angular) {
-  'use strict';
-  
-  angular.module('newrelic-angular.interceptor.http', [])
-    .factory('httpInterceptor', HttpInterceptorFactory)
-    .config(HttpInterceptorConfig);
     
-  /**
-   * Http Interceptor
-   * Intercepts HTTP response and request errors and logs them to new relic
-   */
-  HttpInterceptorFactory.$inject = ['$q', '$log'];
-  function HttpInterceptorFactory($q, $log) {
-
-    // Intercept request and response errors
-    return {
-      requestError: logAndReturnError.bind(null, 'request'),
-      responseError: logAndReturnError.bind(null, 'response')
-    };
-
-    /**
-     * Log and return rejection promise
-     */
-    function logAndReturnError(httpType, rejection) {
-      var config = rejection.config || {};
-      var err = 'bad ' + config.method + ' ' + httpType + ' ' + rejection.config.url;
-      $log.error(new Error(err));
-      return $q.reject(rejection);
+    function argToErrorObject(arg) {
+      try {
+        throw new Error(arg);
+      } catch (e) {
+        return e;
+      }
     }
+  
+  }(window.angular));
 
-  }
+  'use strict';
+  (function(angular) {
+    
+    angular.module('newrelic-angular.interceptor.http', [])
+      .factory('httpInterceptor', HttpInterceptorFactory)
+      .config(HttpInterceptorConfig);
+      
+    /**
+     * Http Interceptor
+     * Intercepts HTTP response and request errors and logs them to new relic
+     * @param {Promise} $q
+     * @param {Object} $log
+     * @return Object
+     */
+    HttpInterceptorFactory.$inject = ['$q', '$log'];
+    function HttpInterceptorFactory($q, $log) {
   
-  /**
-   * Intercept any Http response or request errors
-   *   and log them to new relic
-   */
-  HttpInterceptorConfig.$inject = ['$httpProvider'];
-  function HttpInterceptorConfig($httpProvider) {
-    $httpProvider.interceptors.push('httpInterceptor');
-  }
+      // Intercept request and response errors
+      return {
+        requestError: logAndReturnError.bind(null, 'request'),
+        responseError: logAndReturnError.bind(null, 'response')
+      };
+      
+     /**
+      * Log and and reject promise
+      * @param {String} httpType response or request
+      * @param {Object} rejection contains $http parameter configuration values
+      * @return {Promise} promise
+      */
+      function logAndReturnError(httpType, rejection) {
+        var config = rejection.config || {};
+        var err = 'bad ' + config.method + ' ' + httpType + ' ' + rejection.config.url;
+        $log.error(new Error(err));
+        return $q.reject(rejection);
+      }
   
-})(window.angular);
+    }
+    
+    /**
+     * Intercept any Http response or request errors
+     *   and log them to new relic
+     */
+    HttpInterceptorConfig.$inject = ['$httpProvider'];
+    function HttpInterceptorConfig($httpProvider) {
+      $httpProvider.interceptors.push('httpInterceptor');
+    }
+    
+  }(window.angular));
 
 /**
  * @license Angulartics v0.17.2
