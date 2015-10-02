55ed61802e44591f228446db70dbf976cbcf8b81
Preston Van Loon <preston@gestotech.com>
2015-10-02 13:15:26 -0500

    used isFinite to determine if value is a number

--- dist/newrelic-angular.js
+++ dist/newrelic-angular.js
@@ -8,76 +8,6 @@
   ]);
 
   'use strict';
-  (function(angular) {
-    
-    angular.module('newrelic-angular.interceptor.http', [])
-      .provider('httpInterceptor', HttpInterceptorProvider)
-      .config(HttpInterceptorConfig);
-      
-    /**
-     * Http Interceptor
-     * Intercepts HTTP response and request errors and logs them to new relic
-     * @param {Promise} $q
-     * @param {Object} $log
-     * @return Object
-     */
-    
-    function HttpInterceptorProvider() {
-       
-      var statusesToIgnore = [];
-      
-      this.setStatusesToIgnore = setStatusesToIgnore;
-      
-      function setStatusesToIgnore(statuses) {
-        statusesToIgnore = statuses || [];
-      }
-      
-      function shouldIgnoreStatus(status) {
-        status = +status;
-        return status && statusesToIgnore.indexOf(status) > -1;
-      }
-      
-      function HttpInterceptorFactory($q, $log) {
-        
-        // Intercept request and response errors
-        return {
-          requestError: logAndReturnError.bind(null, 'request'),
-          responseError: logAndReturnError.bind(null, 'response')
-        };
-        
-        /**
-        * Log and and reject promise
-        * @param {String} httpType response or request
-        * @param {Object} rejection contains $http parameter configuration values
-        * @return {Promise} promise
-        */
-        function logAndReturnError(httpType, rejection) {
-          var config = rejection.config || {};
-          
-          if ( ! shouldIgnoreStatus(rejection.status) ) {
-            var err = 'bad ' + config.method + ' ' + httpType + ' ' + rejection.config.url;
-            $log.error(new Error(err));
-          }
-          return $q.reject(rejection);
-        }
-      }
-      
-      this.$get = ['$q', '$log', HttpInterceptorFactory];
-  
-    }
-    
-    /**
-     * Intercept any Http response or request errors
-     *   and log them to new relic
-     */
-    HttpInterceptorConfig.$inject = ['$httpProvider'];
-    function HttpInterceptorConfig($httpProvider) {
-      $httpProvider.interceptors.push('httpInterceptor');
-    }
-    
-  }(window.angular));
-
-  'use strict';
   /*
   * Angular exceptionHandler delegates unhandled exceptions to $log.error
   * This module reports errors to New Relic Browser API
@@ -132,6 +62,75 @@
       }
     }
   
+  }(window.angular));
+
+  'use strict';
+  (function(angular) {
+
+    angular.module('newrelic-angular.interceptor.http', [])
+      .provider('httpInterceptor', HttpInterceptorProvider)
+      .config(HttpInterceptorConfig);
+
+    /**
+     * Http Interceptor
+     * Intercepts HTTP response and request errors and logs them to new relic
+     * @param {Promise} $q
+     * @param {Object} $log
+     * @return Object
+     */
+
+    function HttpInterceptorProvider() {
+
+      var statusesToIgnore = [];
+
+      this.setStatusesToIgnore = setStatusesToIgnore;
+
+      function setStatusesToIgnore(statuses) {
+        statusesToIgnore = statuses || [];
+      }
+
+      function shouldIgnoreStatus(status) {
+        return isFinite(status) && statusesToIgnore.indexOf(status) > -1;
+      }
+
+      function HttpInterceptorFactory($q, $log) {
+
+        // Intercept request and response errors
+        return {
+          requestError: logAndReturnError.bind(null, 'request'),
+          responseError: logAndReturnError.bind(null, 'response')
+        };
+
+        /**
+        * Log and and reject promise
+        * @param {String} httpType response or request
+        * @param {Object} rejection contains $http parameter configuration values
+        * @return {Promise} promise
+        */
+        function logAndReturnError(httpType, rejection) {
+          var config = rejection.config || {};
+
+          if ( ! shouldIgnoreStatus(rejection.status) ) {
+            var err = 'bad ' + config.method + ' ' + httpType + ' ' + rejection.config.url;
+            $log.error(new Error(err));
+          }
+          return $q.reject(rejection);
+        }
+      }
+
+      this.$get = ['$q', '$log', HttpInterceptorFactory];
+
+    }
+
+    /**
+     * Intercept any Http response or request errors
+     *   and log them to new relic
+     */
+    HttpInterceptorConfig.$inject = ['$httpProvider'];
+    function HttpInterceptorConfig($httpProvider) {
+      $httpProvider.interceptors.push('httpInterceptor');
+    }
+
   }(window.angular));
 
 /**
